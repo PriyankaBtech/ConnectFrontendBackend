212acb59b19dbb2be23c3c08e389376b26cb6c50
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-12-24 22:31:41 +0530

    Create jokes api

--- Backend/server.js
+++ Backend/server.js
@@ -2,11 +2,46 @@ import express from 'express'
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send("Server is Ready")
+app.get('/jokes', (req, res) => {
+
+    const jokes = [
+        {
+            id: 1,
+            title: "first joke",
+            content: "this is joke first"
+
+        },
+        {
+            id: 2,
+            title: "second joke",
+            content: "this is joke second"
+
+        },
+        {
+            id: 3,
+            title: "third joke",
+            content: "this is joke third"
+
+        },
+        {
+            id: 4,
+            title: "forth joke",
+            content: "this is joke forth"
+
+        },
+        {
+            id: 5,
+            title: "fifth joke",
+            content: "this is joke fifth"
+
+        }
+    ]
+
+    res.send(jokes)
+    
 })
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4001
 
 app.listen(port, () => {
     console.log(`Server is listening at port: ${port}`)
