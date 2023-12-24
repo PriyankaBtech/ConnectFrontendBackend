b1564bb803f5076c7d39a344aa683d3cb738e723
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-12-24 22:17:00 +0530

    Create server

---
+++ Backend/server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send("Server is Ready")
+})
+
+const port = process.env.PORT || 4000
+
+app.listen(port, () => {
+    console.log(`Server is listening at port: ${port}`)
+})
